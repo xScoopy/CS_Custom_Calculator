32052002e0c128cfcac50a7c87ceeb87a43143f8
xScoopy <JLeary070@gmail.com>
2020-09-01 12:12:02 -0700

    calculations for dps and overall damage completed, with print statements to the user about what their damage numbers look like

--- Custom_Calculator.py
+++ Custom_Calculator.py
@@ -4,15 +4,24 @@ name = input("First, what is your name?")
 
 print("Okay, " + name + ", I have some questions about the damage values of your gear.")
 
-helm_dmg = float(input("What is the damage value on your helmet?")) 
+helm_dmg = float(input("What is the damage value on your helmet? ")) 
 
-chest_dmg = float(input("Thank you. Now what is the damage value on your chestpiece?"))
+chest_dmg = float(input("Thank you. Now what is the damage value on your chestpiece? "))
 
-pants_dmg = float(input("Perfect, and how about the damage value on your pants?"))
+pants_dmg = float(input("Perfect, and how about the damage value on your pants? "))
 
-weapon_dmg = float(input("And finally, what is your weapon's damage value?"))
+weapon_dmg = float(input("And finally, what is your weapon's damage value? "))
 
 total_gear_dmg = helm_dmg + chest_dmg + pants_dmg + weapon_dmg
 
-boss_fight_duration = float(input("How many minutes will the next boss fight last?"))
+boss_fight_duration_min = float(input("How many minutes will the next boss fight last? "))
 
+boss_fight_duration_sec = boss_fight_duration_min * 60
+
+dps_boss_fight = total_gear_dmg / boss_fight_duration_sec
+
+print(f"{name}, your damage per second for this boss fight will be {dps_boss_fight}")
+
+total_bossfight_dmg = dps_boss_fight * boss_fight_duration_sec
+
+print(f"This comes to a grand total of {total_bossfight_dmg} damage done throughout the encounter!")
